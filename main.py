04d22fd834be9e8158cb2ec0f7b9b52dd03d971a
Alexandre Aubin <alexandre.aubin@cern.ch>
2014-06-09 03:03:53 +0200

    Adding plot generation$

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
 
+from matplotlib import pyplot as plotting
+import numpy as math
 from xml.dom import minidom
 import subprocess
 import codecs
 import time
+import os
 
-'''
-items = ['Tritanium' ]
-systems = ['Jita', 'Rens' ]
-'''
 
 items = ['Tritanium', 'Pyerite', 'Mexallon', 'Isogen', 'Nocxium', 'Zydrine', 'Megacyte', 'Heavy Water', 'Liquid Ozone', 'Helium Isotopes', 'Strontium Clathrates', 'Oxygen Isotopes', 'Nitrogen Isotopes', 'Hydrogen Isotopes']
-systems = ['Jita', 'Amarr', 'Rens', 'Oursulaert', 'Dodixie']
+systems = ['Jita', 'Amarr', 'Rens', 'Dodixie']
+colors  = { 'Jita' : '#000000',
+            'Amarr' : '#FFBF00', 
+            'Rens' : '#FF6100', 
+            'Dodixie' : '#37AEE5',
+           }
+'''            
+            'Oursulaert' : '#01DB55', 
+'''
 
+'''
+items = ['Tritanium', 'Pyerite', 'Mexallon', 'Isogen', 'Nocxium', 'Zydrine', 'Megacyte', 'Heavy Water', 'Liquid Ozone', 'Helium Isotopes', 'Strontium Clathrates', 'Oxygen Isotopes', 'Nitrogen Isotopes', 'Hydrogen Isotopes']
+systems = ['Jita', 'Amarr', 'Rens', 'Oursulaert', 'Dodixie']
+'''
 def analyzeOrders(date, item, system) :
 
     ''' Read input '''
@@ -27,7 +38,6 @@ def analyzeOrders(date, item, system) :
     buy_orders_list = []
     sell_orders_list = []
 
-    print len(sell_orders)
     for order in sell_orders :
         ''' Filter low-sec systems '''
         if (float(order.getElementsByTagName("security")[0].childNodes[0].nodeValue) < 0.5) :
@@ -95,7 +105,7 @@ def analyzeOrders(date, item, system) :
         sell_priceMeanBegin /= sell_volumeBegin
     else :
         sell_priceMeanBegin = 0
-
+    
     buy_ordersBegin = []
     buy_volumeBegin = 0;
     buy_priceMeanBegin = 0;
@@ -106,18 +116,23 @@ def analyzeOrders(date, item, system) :
         buy_ordersBegin.append(order)
         buy_priceMeanBegin += order[0] * order[1]
         buy_volumeBegin += order[1]
-    buy_priceMeanBegin /= buy_volumeBegin
 
     if (buy_volumeBegin != 0) :
         buy_priceMeanBegin /= buy_volumeBegin
     else :
         buy_priceMeanBegin = 0
-
+    
     ''' Compute mean '''
 
-    sellAndBuy_priceMean = (buy_priceMeanBegin + sell_priceMeanBegin) / 2;
+    if (buy_priceMeanBegin == 0) :
+        buy_priceMeanBegin = sell_priceMeanBegin;
+    if (sell_priceMeanBegin == 0) :
+        sell_priceMeanBegin = buy_priceMeanBegin;
 
-    return (buy_priceMeanBegin, sellAndBuy_priceMean, sell_priceMeanBegin)
+    priceMean = (buy_priceMeanBegin + sell_priceMeanBegin) / 2;
+    priceGap  = abs((buy_priceMeanBegin - sell_priceMeanBegin)) / 2;
+
+    return (priceMean, priceGap)
 
 
 def fetchNewData(itemsList, systemsList) :
@@ -149,23 +164,75 @@ def fetchNewData(itemsList, systemsList) :
             p = subprocess.Popen(["wget", "--no-verbose", "-O", "data/"+item+"/"+system+".xml", url])
             p.wait()
 
-    date = time.strftime("%d_%m_%Y_%H:%M:%S")
+    date = time.strftime("%Y_%m_%d_%H:%M:%S")
     p = subprocess.Popen(["mv", "data", "history/"+date])
     p.wait()
+
 
 '''
 fetchNewData(items, systems)
 '''
-testDate="20_05_2014_23:46:24"
 
-print "+-----------------------------------------------------------------------------------------------------------------------+"
-print "|\tSystem\t|\tBuy mean\t|\tBuy 5% mean\t|\t  Mean  \t|\tSell 5% mean\t|\tSell mean\t|"
+plotting.clf()
+plotting.hold(1)
+
+ref = systems[0]
+timeAxis = {}
+priceCurve_mean = {}
+priceCurve_sell = {}
+priceCurve_buy  = {}
+
+priceCurve_meanGain = {}
+priceCurve_sellGain = {}
+priceCurve_buyGain  = {}
+
+history = sorted(os.listdir("./history/"))
 for item in items :
     for system in systems :
-        print "test"
-        print item, system
-        (b,m,s) = analyzeOrders(testDate,item,system)
-        print b, m, s
+        timeAxis[item, system] = []
+        priceCurve_mean[item, system] = []
+        priceCurve_sell[item, system] = []
+        priceCurve_buy [item, system] = []
+        priceCurve_meanGain[item, system] = []
+        priceCurve_sellGain[item, system] = []
+        priceCurve_buyGain [item, system] = []
 
+        for (i, record) in enumerate(history):
+            timeAxis[item, system].append(int(int(time.mktime(time.strptime(record,"%Y_%m_%d_%H:%M:%S")) - time.time()) / 864.0)/100.0)
+            (p,g) = analyzeOrders(record,item,system)
+            p = int(p * 100)/100.0
+            g = int(g * 100)/100.0
+            priceCurve_mean[item, system].append(p)
+            priceCurve_sell[item, system].append(p+g)
+            priceCurve_buy [item, system].append(p-g)
+            
+            priceCurve_meanGain[item, system].append(priceCurve_mean[item, system][i] / priceCurve_mean[item, ref][i])
+            priceCurve_sellGain[item, system].append(priceCurve_sell[item, system][i] / priceCurve_mean[item, ref][i])
+            priceCurve_buyGain [item, system].append(priceCurve_buy [item, system][i] / priceCurve_mean[item, ref][i])
 
+            print item, "@", system, "on", record,  p, g
+        '''
+        plotting.plot(timeAxis[item, system], priceCurve_mean[item, system], 'k', color=colors[system], linewidth=2.5)
+        plotting.fill_between(timeAxis[item, system], priceCurve_sell[item, system], priceCurve_buy[item, system], alpha=0.2, edgecolor=colors[system], facecolor=colors[system])
+        print timeAxis
+        print priceMean
+        '''
 
+for item in items :
+    plotting.figure()
+    plotting.subplot(2, 1, 1)
+    plotting.plot(timeAxis[item, ref], priceCurve_mean[item, ref], 'yo-', color=colors[ref], linewidth=1.5)
+    plotting.fill_between(timeAxis[item, ref], priceCurve_sell[item, ref], priceCurve_buy[item, ref], alpha=0.1, edgecolor=colors[ref], facecolor=colors[ref])
+    plotting.ylabel(ref+' price')
+    axes = plotting.gca()
+    axes.grid(True)
+
+    plotting.subplot(2, 1, 2)
+    for system in systems :
+        plotting.ylabel('Profit margin')
+        plotting.plot(timeAxis[item, system], priceCurve_meanGain[item, system], 'r-', color=colors[system], linewidth=1.5)
+        plotting.fill_between(timeAxis[item, system], priceCurve_sellGain[item, system], priceCurve_buyGain[item, system], alpha=0.2, edgecolor=colors[system], facecolor=colors[system])
+        axes = plotting.gca()
+        axes.grid(True)
+    
+    plotting.savefig(item+'.png', bbox_inches='tight')
